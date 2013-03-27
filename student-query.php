2b78bf4be2480961464bbecac13f4665e4e47df1
Dahmian Owen <dahmian.owen@mellmo.com>
2013-03-27 13:31:56 -0700

    Created student query file

---
+++ student-query.php
@@ -0,0 +1,26 @@
+<?php
+require 'filter-by-column-value.php';
+
+function filterBySection($registrantArray, $section) {
+  return filterByColumnValue($registrantArray, array('District/Section'), array($section));
+}
+
+function getStudentsWhoRequireTranscripts($registrantArray) {
+    return filterByColumnValue($registrantArray, array('Event1', 'Event2'), array("Accounting I"));
+}
+
+function getStudentsWhoRequireGradeProof($registrantArray) {
+   $eventsThatRequireProofOfGrade = array(
+     'Business Math',
+     'Creed',
+     'FBLA Principles and Procedures',
+     'Introduction to Business',
+     'Introduction to Business Communication',
+     'Introduction to Parliamentary Procedure',
+     'Introduction to Technology Concepts',
+     'Word Processing I',
+     'Public Speaking I'
+   );
+   return filterByColumnValue($registrantArray, array('Event1', 'Event2'), $eventsThatRequireProofOfGrade);
+}
+?>
--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?php
-require 'filter-by-column-value.php';
+require 'student-query.php';
 require 'render-students.php';
 require 'convert-tab-to-array.php';
 
@@ -8,27 +8,4 @@ $data = getStudentsWhoRequireGradeProof(
     filterBySection($tabArray, 'Alpha')
 );
 renderStudents($data);
-
-function filterBySection($registrantArray, $section) {
-  return filterByColumnValue($registrantArray, array('District/Section'), array($section));
-}
-
-function getStudentsWhoRequireTranscripts($registrantArray) {
-    return filterByColumnValue($registrantArray, array('Event1', 'Event2'), array("Accounting I"));
-}
-
-function getStudentsWhoRequireGradeProof($registrantArray) {
-   $eventsThatRequireProofOfGrade = array(
-     'Business Math',
-     'Creed',
-     'FBLA Principles and Procedures',
-     'Introduction to Business',
-     'Introduction to Business Communication',
-     'Introduction to Parliamentary Procedure',
-     'Introduction to Technology Concepts',
-     'Word Processing I',
-     'Public Speaking I'
-   );
-   return filterByColumnValue($registrantArray, array('Event1', 'Event2'), $eventsThatRequireProofOfGrade);
-}
 ?>
