e98648331141a95799eceb9c5bf143761e745aaf
Dahmian Owen <dahmian.owen@mellmo.com>
2013-03-27 11:36:20 -0700

    Created a views folder

--- render-students.php
+++ render-students.php
@@ -5,7 +5,7 @@ Mustache_Autoloader::register();
 function renderStudents($data) {
   $template_data['students'] = new ArrayIterator($data); 
   $m = new Mustache_Engine;
-  $template = file_get_contents('test.mustache');
+  $template = file_get_contents('views/students.mustache');
   echo $m->render($template, $template_data);
 }
 ?>
