5c833cb54fa604ec81f08712953b9abfaf0c6b20
Dahmian Owen <dahmian.owen@mellmo.com>
2013-03-27 11:11:51 -0700

    Filtering and rendering are split out into new files

---
+++ render-students.php
@@ -0,0 +1,11 @@
+<?php
+require 'mustache.php/src/Mustache/Autoloader.php';
+Mustache_Autoloader::register();
+
+function renderStudents($data) {
+  $template_data['students'] = new ArrayIterator($data); 
+  $m = new Mustache_Engine;
+  $template = file_get_contents('test.mustache');
+  echo $m->render($template, $template_data);
+}
+?>
--- test.php
+++ test.php
@@ -1,17 +1,13 @@
 <?php
-require 'mustache.php/src/Mustache/Autoloader.php';
-Mustache_Autoloader::register();
+require 'filter-by-column-value.php';
+require 'render-students.php';
+require 'convert-tab-to-array.php';
 
-include 'convert-tab-to-array.php';
 $tabArray = convertTabFileToArray('test.tab');
-
 $data = getStudentsWhoRequireGradeProof(
     filterBySection($tabArray, 'Alpha')
 );
-$template_data['students'] = new ArrayIterator($data); 
-$m = new Mustache_Engine;
-$template = file_get_contents('test.mustache');
-echo $m->render($template, $template_data);
+renderStudents($data);
 
 function filterBySection($registrantArray, $section) {
   return filterByColumnValue($registrantArray, array(12), array($section));
@@ -34,42 +30,5 @@ function getStudentsWhoRequireGradeProof($registrantArray) {
      'Public Speaking I'
    );
    return filterByColumnValue($registrantArray, array(15, 17), $eventsThatRequireProofOfGrade);
-}
-
-function filterByColumnValue($sourceArray, $columnNumbers, $stringMatchs) {
-  foreach($sourceArray as $sourceKey => $sourceValue) {
-    foreach($columnNumbers as $columnKey => $columnValue) {
-      foreach($stringMatchs as $matchKey => $matchValue) {
-        if ($sourceValue[$columnValue] == $matchValue) {
-          $sourceValue = mapColumnNames($sourceValue);
-          $filteredArray["ID-$sourceValue[0]"] = $sourceValue; //if key like an int, PHP will store it like an int
-        }
-      }
-    }
-  }
-  return $filteredArray;
-}
-
-function mapColumnNames($numberedArray) {
-  $map = array(
-    'Student Id',
-    'First Name',
-    'Last Name',
-    'School Name',
-  );
-  foreach($numberedArray as $sourceKey => $sourceValue) {
-    foreach($map as $mapKey => $mapValue) {
-      if ($sourceKey === $mapKey) {
-        $numberedArray = replaceArrayKey($numberedArray, $mapValue, $sourceKey);
-      }
-    }
-  }
-  return $numberedArray;
-
-}
-
-function replaceArrayKey($numberedArray, $newKey, $sourceKey) {
-  $numberedArray[$newKey] = $numberedArray[$sourceKey];
-  return $numberedArray;
 }
 ?>
