fd66f661907215b07ad6403fb0ff19f334af8497
Dahmian Owen <dahmian.owen@mellmo.com>
2013-03-27 11:24:40 -0700

    Mapping now in tab to array conversion

--- convert-tab-to-array.php
+++ convert-tab-to-array.php
@@ -4,8 +4,32 @@ function convertTabFileToArray($fileName) {
   $lines = explode("\n", $fileContents);
   foreach($lines as $key => $value){
     $row = explode("\t", $value);
+    $row = mapColumnNames($row);
     $tabArray[$row[0]] = $row;
   }
   return $tabArray;
+}
+
+function mapColumnNames($numberedArray) {
+  $map = array(
+    'Student Id',
+    'First Name',
+    'Last Name',
+    'School Name',
+  );
+  foreach($numberedArray as $sourceKey => $sourceValue) {
+    foreach($map as $mapKey => $mapValue) {
+      if ($sourceKey === $mapKey) {
+        $numberedArray = replaceArrayKey($numberedArray, $mapValue, $sourceKey);
+      }
+    }
+  }
+  return $numberedArray;
+
+}
+
+function replaceArrayKey($numberedArray, $newKey, $sourceKey) {
+  $numberedArray[$newKey] = $numberedArray[$sourceKey];
+  return $numberedArray;
 }
 ?>
--- filter-by-column-value.php
+++ filter-by-column-value.php
@@ -4,35 +4,11 @@ function filterByColumnValue($sourceArray, $columnNumbers, $stringMatchs) {
     foreach($columnNumbers as $columnKey => $columnValue) {
       foreach($stringMatchs as $matchKey => $matchValue) {
         if ($sourceValue[$columnValue] == $matchValue) {
-          $sourceValue = mapColumnNames($sourceValue);
           $filteredArray["ID-$sourceValue[0]"] = $sourceValue; //if key like an int, PHP will store it like an int
         }
       }
     }
   }
   return $filteredArray;
-}
-
-function mapColumnNames($numberedArray) {
-  $map = array(
-    'Student Id',
-    'First Name',
-    'Last Name',
-    'School Name',
-  );
-  foreach($numberedArray as $sourceKey => $sourceValue) {
-    foreach($map as $mapKey => $mapValue) {
-      if ($sourceKey === $mapKey) {
-        $numberedArray = replaceArrayKey($numberedArray, $mapValue, $sourceKey);
-      }
-    }
-  }
-  return $numberedArray;
-
-}
-
-function replaceArrayKey($numberedArray, $newKey, $sourceKey) {
-  $numberedArray[$newKey] = $numberedArray[$sourceKey];
-  return $numberedArray;
 }
 ?>
